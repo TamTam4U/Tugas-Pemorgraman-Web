946a4d18b70c4a6f3d0207c456571fb9d3bd3b36
Moch Pratama Hidayat <100699676+TamTam4U@users.noreply.github.com>
2022-10-06 19:43:06 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Halaman Login</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+   
+    <body>
+        <div id="grad"></div>
+        <div class="container">
+          <h1>Login</h1>
+            <form method="GET" action="proses.php">
+                <label>Username</label><br>
+                <input type="text" name="user"><br>
+                <label>Password</label><br>
+                <input type="password" name="pass"><br>
+                <button>Log in</button>
+            </form>
+        </div>     
+    </body>
+</html>
